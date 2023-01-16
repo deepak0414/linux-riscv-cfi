cb59313be60e09d456e366b6dc3de763922b216d
Deepak Gupta <debug@rivosinc.com>
2023-01-15 23:28:54 -0800

    riscv: zisslpcfi enumeration
    This patch adds support for detecting zisslpcfi. zisslpcfi stands for
    unprivleged integer spec extension to support shadow stack and landing
    pad instruction for indirect branch.
    
    This patch looks for "zisslpcfi" in device tree and accordinlgy lights up
    bit in cpu feature bitmap. Furthermore this patch adds detection utility
    functions to return whether shadow stack or landing pads are supported by
    cpu.
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>

--- arch/riscv/kernel/cpufeature.c
+++ arch/riscv/kernel/cpufeature.c
@@ -205,6 +205,7 @@ void __init riscv_fill_hwcap(void)
 				SET_ISA_EXT_MAP("zihintpause", RISCV_ISA_EXT_ZIHINTPAUSE);
 				SET_ISA_EXT_MAP("sstc", RISCV_ISA_EXT_SSTC);
 				SET_ISA_EXT_MAP("svinval", RISCV_ISA_EXT_SVINVAL);
+				SET_ISA_EXT_MAP("zisslpcfi", RISCV_ISA_EXT_ZCFI);
 			}
 #undef SET_ISA_EXT_MAP
 		}
